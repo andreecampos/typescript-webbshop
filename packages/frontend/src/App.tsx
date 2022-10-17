678a81629702e885ac8d8306d9d6015fdec9b87d
daigoro14 <daigoropersson@gmail.com>
2022-10-17 21:39:13 +0200

    added routes and fixed proxy between client and server

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import ProductPage from './pages/ProductPage';
+import RegisterPage from './pages/RegisterPage';
+import ShoppingCartPage from './pages/ShoppingCartPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/register" element={<RegisterPage/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/" element={<ProductPage/>}/>
+            <Route path="/cart" element={<ShoppingCartPage/>}/>
+          </Routes>
+        </Router>
+      </div>
   );
 }
 
