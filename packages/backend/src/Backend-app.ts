632a7da141029d3fac5b18d026defc63584cb763
sandro <akecakabro@gmail.com>
2022-10-18 10:51:12 +0200

    test conflicts

--- packages/backend/src/Backend-app.ts
+++ packages/backend/src/Backend-app.ts
@@ -4,7 +4,7 @@ const app: Application = express();
 app.use(cors());
 app.use(json());
 const port: number = parseInt(process.env.SERVER_PORT || "3001");
-app.get("/hello", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 app.listen(port, function () {
