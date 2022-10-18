edd5ba4fb3b2709b17b21541a66dc311906c4515
andreecampos <orlandoandree1998@gmail.com>
2022-10-18 19:21:57 +0200

    Prettier

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,24 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
-import ProductsPage from './pages/ProductsPage'
-import './App.css';
-import { ProductDetail } from './pages/ProductDetail';
-
+import { Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import "./App.css";
+import { ProductDetail } from "./pages/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-
       <Routes>
-
         <Route path="/products" element={<ProductsPage />}></Route>
-        <Route path="/details/:id" element={<ProductDetail  />}></Route>
-
-        
-      </Routes> 
-
+        <Route path="/details/:id" element={<ProductDetail />}></Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
- 
