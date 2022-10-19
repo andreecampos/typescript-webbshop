b8d6e931c76798e5d29fedbee94a1679fc6b46a4
sandro <akecakabro@gmail.com>
2022-10-19 18:13:17 +0200

    add user to mongoDB

--- packages/backend/src/Controllers/User_Controllers.ts
+++ packages/backend/src/Controllers/User_Controllers.ts
@@ -1,13 +1,24 @@
 import express, { Router, Request, Response } from "express";
 import { user_interface } from "@webbshop-app/shared";
+import { saveUser } from "../Models/userModel";
+
 const User_Controller = express.Router();
 
 User_Controller.post(
   "/",
-  (req: Request<user_interface>, res: Response<user_interface>) => {
-    const query = req.body;
-    console.log(query);
-    res.send(req.body);
+  async (
+    req: Request<user_interface>,
+    res: Response<user_interface[] | string>
+  ) => {
+    const userCreate = req.body;
+    console.log(userCreate);
+    try {
+      await saveUser(req.body);
+      res.send("ok");
+    } catch {
+      console.log("error user");
+      res.send("wrong user");
+    }
   }
 );
 
