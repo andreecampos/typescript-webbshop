d919eba0751bf4c096c84538323e8ce3cdf3e8e1
sandro <akecakabro@gmail.com>
2022-10-21 16:55:00 +0200

    add user info fields

--- packages/backend/src/Models/userModel.ts
+++ packages/backend/src/Models/userModel.ts
@@ -4,6 +4,9 @@ const bcrypt = require("bcrypt");
 
 const userModel = new Schema({
   username: { type: String, required: true, unique: true },
+  mail: { type: String, required: true, unique: true },
+  phoneNr: { type: String, required: true },
+  address: { type: String, required: true },
   password: { type: String, required: true },
 });
 
